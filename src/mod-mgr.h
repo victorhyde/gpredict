435e65314c7771a1cf355352f85cea3e29904cba
Victor <victor_hyde@outlook.com>
2020-03-10 16:08:07 -0700

    command line options to trigger auto engage

--- src/mod-mgr.h
+++ src/mod-mgr.h
@@ -3,6 +3,9 @@
 
 GtkWidget      *mod_mgr_create(void);
 gint            mod_mgr_add_module(GtkWidget * module, gboolean dock);
+GtkWidget      *mod_mgr_get_module(gchar * name);
+GtkWidget      *mod_mgr_get_current_module();
+void            mod_mgr_switch_to_module(GtkWidget * module);
 gint            mod_mgr_remove_module(GtkWidget * module);
 void            mod_mgr_save_state(void);
 gboolean        mod_mgr_mod_is_visible(GtkWidget * module);
